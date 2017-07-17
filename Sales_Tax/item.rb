bc4a80a5b7707d711fc94d5c01bc82be4fbebcca
Nitin <nnks5758@gmail.com>
2017-07-17 12:24:21 +0530

    Sales Tax after changes made

--- Sales_Tax/item.rb
+++ Sales_Tax/item.rb
@@ -1,13 +1,6 @@
-require_relative 'category_info'
-require_relative 'is_imported'
-require_relative 'amount'
+require_relative 'sales_tax'
 
-class Item < Amount
-  include CategoryInfo
-  include IsImported
-
-  @quantity = []
-  @price  = []
+class Item < SalesTax
   
   def input
     @item_list = []
@@ -20,12 +13,38 @@ class Item < Amount
   end
   
   def attributes
+    @quantity = []
+    @price  = []
+    
     @quantity = @item_list.map(& :first)
     @price = @item_list.map(& :last)
     
     is_tax_free?
     is_imported?
   end
+
+  def is_tax_free?
+    @taxable = []
+    @words = ["book","books","chocolate","chocolates","pills","medicine","medicines","food"]
+    
+    @item_list.length.times do |index|
+      @taxable[index] = (@item_list[index] & @words).empty?
+    end
+  end
+
+  def is_imported?
+    @import = []
+    @item_list.length.times do |index|
+      @import[index] = @item_list[index].include?("imported")
+    end
+  end
+
+  def updated_price
+    @item_list.length.times do |index|
+      @item_list[index][-1] = @price[index].round(2)
+    end
+  end
+
 end
 
 item = Item.new
@@ -33,4 +52,5 @@ item = Item.new
 item.input
 item.attributes
 item.tax_calculator
+item.updated_price
 item.show_output
--- Sales_Tax/sales_tax.rb
+++ Sales_Tax/sales_tax.rb
@@ -1,19 +1,19 @@
-class Amount
-  
-  @@total_sales_tax = 0.0
+class SalesTax 
 
   def tax_calculator
+    @total_sales_tax = 0
+    
     @price.length.times do |index|
-      sum = 0.0
+      sum = 0
       
-      if @category[index] == false && @import[index] == true
+      if @taxable[index] == false && @import[index] == true
         sum = sum + (@quantity[index].to_i*@price[index].to_f*0.05) 
         sales_tax_price(index,sum)
       
-      elsif @category[index] == false && @import[index] == false
+      elsif @taxable[index] == false && @import[index] == false
         sales_tax_price(index,sum)
       
-      elsif @category[index] == true && @import[index] == false
+      elsif @taxable[index] == true && @import[index] == false
         sum = sum + ((@quantity[index].to_f*@price[index].to_f)*0.10)
         sales_tax_price(index,sum)
       
@@ -25,32 +25,29 @@ class Amount
   end
   
   def sales_tax_price (index,sum)
-    @@total_sales_tax = @@total_sales_tax + sum
+    @total_sales_tax = @total_sales_tax + sum
     @price[index] = @price[index].to_f + sum
-  end
-
-  def updated_price
-    @item_list.length.times do |index|
-      @item_list[index][-1] = @price[index].round(2)
-    end
   end
 
   def total_amount
     total = 0
+    
     @price.each do |element|
       total = total + element
     end
+    
     total
   end
 
   def show_output
     updated_price
+    
     puts "\nOutput is : "
     @item_list.each do |element|
       puts "  #{element.map {|value| value == 'at' ? ':' : value }.join (" ")}\n"
     end
 
-    puts "\n  Sales Tax : #{@@total_sales_tax.round(2)}"
+    puts "\n  Sales Tax : #{@total_sales_tax.round(2)}"
     puts "  Total : #{total_amount.round(2)}"
   end
 end
